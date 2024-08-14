bbce88ea322c3d5d7aafa26184780348acb4f518
Asif A Khuda <asif13.aak@gmail.com>
2024-08-14 21:22:00 +0600

    vercel 5

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,19 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 MongoDB();
-
+const allowedOrigins = [
+    frontend,  // or your frontend URL during development
+    'https://aust-carnival.vercel.app'
+];
 
 app.use(cors({
-    origin: "https://aust-carnival.vercel.app",
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ['Content-Type', 'authToken'],
     credentials: true

f15dc5ee810564a175c12c0268647ea6d546140b
Asif A Khuda <asif13.aak@gmail.com>
2024-08-30 10:35:10 +0600

    3

--- frontend/src/Screens/Users/Home.jsx
+++ frontend/src/Screens/Users/Home.jsx
@@ -7,24 +7,13 @@ import Home_WorkShop from "../../Components/Home_WorkShop";
 import WorkshopList from "../../Components/admin/WorkshopList";
 export default function Home() {
   const isAdmin = localStorage.getItem("adminStatus");
-  const [width, setWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-
-    // Add event listener to update width on resize
-    window.addEventListener("resize", handleResize);
-
-    // Clean up the event listener when the component unmounts
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
   return (
     <div className="home">
       <Navbar />
       {isAdmin === "false" || isAdmin === null ? (
         <>
           <Home_Event />
-          <p>Screen width: {width}px</p>
           <Home_WorkShop />
         </>
       ) : (
